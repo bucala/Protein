f1c0f5a98ae291f4dc477f72a45db02a4d5134f1
bucala <marcel.bucala@gmail.com>
2026-05-26 22:30:51 +0200

    Create img.js (#4)

---
+++ api/img.js
@@ -0,0 +1,15 @@
+export default async function handler(req, res) {
+  const { url } = req.query;
+  if (!url) return res.status(400).end();
+  
+  const response = await fetch(decodeURIComponent(url), {
+    headers: { 'Referer': 'https://gymbeam.sk/' }
+  });
+  
+  if (!response.ok) return res.status(404).end();
+  
+  const buffer = await response.arrayBuffer();
+  res.setHeader('Content-Type', response.headers.get('content-type') || 'image/jpeg');
+  res.setHeader('Cache-Control', 'public, max-age=86400');
+  res.end(Buffer.from(buffer));
+}
